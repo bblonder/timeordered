49658f5882bfdbfc08abe19900886a8f3f650399
bblonder <bblonder@gmail.com>
2018-07-03 22:47:42 -0600

    added igraph plotting options. updated package for namespace issues.

--- R/plotnetworkslices.R
+++ R/plotnetworkslices.R
@@ -1,15 +1,11 @@
-plotnetworkslices <-
-function(slices, timedeltas)
+plotnetworkslices <- function (slices, timedeltas, ...) 
 {
-	timestrings <- paste(timedeltas[,1]," - ", timedeltas[,2])
-	
-	par(mfrow=c(ceiling(sqrt(length(slices))),ceiling(sqrt(length(slices)))))
-	par(mar=c(1,1,1,1))
-	for (i in 1:length(slices)) 
-	{ 
-		plottanet(slices[[i]])
-		text(0,0,timestrings[i]) 
-	}
-
+  timestrings <- paste(timedeltas[, 1], " - ", timedeltas[, 
+                                                          2])
+  par(mfrow = c(ceiling(sqrt(length(slices))), ceiling(sqrt(length(slices)))))
+  par(mar = c(1, 1, 1, 1))
+  for (i in 1:length(slices)) {
+    plottanet(slices[[i]],...)
+    text(0, 0, timestrings[i])
+  }
 }
-
